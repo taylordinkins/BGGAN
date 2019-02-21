c83129416d5e79c50cbe873f1975d065b7acf106
Taylor Dinkins <taylordinkins@Taylors-MacBook-Pro.local>
2019-02-21 13:30:18 -0800

    CausalGAN Bayes Model

---
+++ causal_gan_model.py
@@ -0,0 +1,28 @@
+
+# coding: utf-8
+
+
+import numpy as np
+import pandas as pd
+from pgmpy.models import BayesianModel
+
+pd.set_option('display.max_columns', 500)
+
+
+atts = pd.read_csv('list_attr_celeba.csv')
+atts = atts[['Young', 'Male', 'Eyeglasses', 'Bald', 'Mustache', 'Smiling', 'Wearing_Lipstick', 'Mouth_Slightly_Open', 'Narrow_Eyes']]
+
+
+graph = BayesianModel()
+graph.add_nodes_from(atts.columns)
+
+graph.add_edges_from([('Young', 'Eyeglasses'), ('Young', 'Bald'), ('Young', 'Mustache'), ('Male', 'Mustache'), 
+                      ('Male', 'Smiling'), ('Male', 'Wearing_Lipstick'), ('Young', 'Mouth_Slightly_Open'), 
+                      ('Young', 'Narrow_Eyes'), ('Male', 'Narrow_Eyes'), ('Smiling', 'Narrow_Eyes'), 
+                      ('Smiling', 'Mouth_Slightly_Open'), ('Young', 'Smiling')])
+
+graph.fit(atts)
+
+for cpd in graph.get_cpds():
+    print(cpd)
+
