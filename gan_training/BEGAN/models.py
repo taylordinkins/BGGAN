56fe063ef4eeb41f1de515b86f9691622772f629
neale <ratzlafn@oregonstate.edu>
2019-03-15 17:40:21 -0700

    adding BEGAN

---
+++ gan_training/BEGAN/models.py
@@ -0,0 +1,136 @@
+import torch.nn as nn
+import torch.nn.functional as F
+
+class Generator(nn.Module):
+    def __init__(self, args, disc=False):
+        for k, v in vars(args).items():
+            setattr(self, k, v)
+        super(Generator, self).__init__()
+
+        self.conv_block = nn.Sequential([
+            nn.Linear(self.h, 8*8*self.nc),
+            nn.ELU(inplace=True),
+            nn.Conv2d(self.nc, self.nc, 3, 1, 1),
+            nn.ELU(inplace=True),
+            nn.Conv2d(self.nc, self.nc, 3, 1, 1),
+            nn.ELU(inplace=True),
+            F.interpolate(scale_factor=2),
+            nn.Conv2d(self.nc, self.nc, 3, 1, 1),
+            nn.ELU(inplace=True),
+            nn.Conv2d(self.nc, self.nc, 3, 1, 1),
+            nn.ELU(inplace=True),
+            F.interpolate(scale_factor=2),
+            nn.Conv2d(self.nc, self.nc, 3, 1, 1),
+            nn.ELU(inplace=True),
+            nn.Conv2d(self.nc, self.nc, 3, 1, 1),
+            nn.ELU(inplace=True),
+            F.interpolate(scale_factor=2),
+            nn.Conv2d(self.nc, self.nc, 3, 1, 1),
+            nn.ELU(inplace=True),
+            nn.Conv2d(self.nc, self.nc, 3, 1, 1),
+            nn.ELU(inplace=True),
+        ])
+        self.last_conv = nn.Conv2d(self.nc, 3, 3, 1, 1)
+
+    def forward(self, input):
+        x = self.l0(input)
+        x = x.view(self.batch_size, self.nc, 8, 8)
+        x = self.conv_block(x)
+        x = self.last_conv(x)
+        x = torch.tanh(x)
+        return x
+    
+class Encoder(nn.Module):
+    def __init__(self, args):
+        for k, v in vars(args).items():
+            setattr(self, k, v)
+        super(Encoder, self).__init__()
+        self.block1 = nn.Sequential([
+            nn.Conv2d(3, self.nc, 3, 1, 1),
+            nn.ELU(inplace=True),
+            nn.Conv2d(self.nc, self.nc, 3, 1, 1),
+            nn.ELU(inplace=True),
+            nn.Conv2d(self.nc, self.nc, 3, 1, 1),
+            nn.ELU(inplace=True),
+            nn.Conv2d(self.nc, self.nc, 1, 1, 0),
+        ])
+        self.pool1 = nn.AvgPool2d(2, 2)
+
+        self.block2 = nn.Sequential([
+            nn.Conv2d(self.nc, self.nc, 3, 1, 1),
+            nn.ELU(inplace=True),
+            nn.Conv2d(self.nc, self.nc, 3, 1, 1),
+            nn.ELU(inplace=True),
+            nn.Conv2d(self.nc, 2*self.nc, 1, 1, 0),
+        ])
+        self.pool2 = nn.AvgPool2d(2, 2)
+
+        self.block3 = nn.Sequential([
+            nn.Conv2d(2*self.nc, 2*self.nc, 3, 1, 1),
+            nn.ELU(inplace=True),
+            nn.Conv2d(2*self.nc, 2*self.nc, 3, 1, 1),
+            nn.ELU(inplace=True),
+            nn.Conv2d(2*self.nc, 3*self.nc, 1, 1, 0),
+        ])
+        self.pool3 = nn.AvgPool2d(2, 2)
+        
+        self.block4 = nn.Sequential([
+            nn.Conv2d(3*self.nc, 3*self.nc, 3, 1, 1),
+            nn.ELU(inplace=True),
+            nn.Conv2d(3*self.nc, 3*self.nc, 3, 1, 1),
+            nn.ELU(inplace=True),
+        ])
+        self.linear1 = nn.Linear(8*8*3*self.nc, 64),
+        self.block5 = nn.Sequential([
+            nn.Conv2d(3*self.nc, 3*self.nc, 3, 1, 1),
+            nn.ELU(inplace=True),
+            nn.Conv2d(3*self.nc, 3*self.nc, 3, 1, 1),
+            nn.ELU(inplace=True),
+            nn.Conv2d(3*self.nc, 4*self.nc, 1, 1, 0),
+            nn.AvgPool2d(2, 2),
+            nn.Conv2d(4*self.nc, 4*self.nc, 3, 1, 1),
+            nn.ELU(inplace=True),
+            nn.Conv2d(4*self.nc, 4*self.nc, 3, 1, 1),
+            nn.ELU(inplace=True),
+        ])
+        self.linear2 = nn.Linear(8*8*4*self.nc, self.z)
+
+        
+    def forward(self, input):
+        x = self.block1(input)
+        x = self.pool1(x)
+        x = self.block2(x)
+        x = self.pool2(x)
+        x = self.block3(x)
+        x = self.pool3(x)
+        if self.scale_size == 64:
+            x = self.block4(x)
+            x = x.view(self.batch_size, 8*8*3*self.nc)
+            x = self.linear1(x)
+        else:
+            x = self.block5(x)
+            x = x.view(self.batch_size, 8*8*4*self.nc)
+            x = F.elu(self.linear2(x), True)
+        return x
+    
+class Discriminator(nn.Module):
+    def __init__(self, nc):
+        super(Discriminator, self).__init__()
+        self.enc = Encoder(nc)
+        self.dec = Decoder(nc, True)
+    def forward(self, input):
+        return self.dec(self.enc(input))
+
+
+def weights_init(self, m):
+    classname = m.__class__.__name__
+    if classname.find('Conv') != -1:
+        m.weight.data.normal_(0.0, 0.02)
+
+class L1_Loss(nn.Module):
+    def __init__(self, size_average=True):
+        super(_Loss, self).__init__()
+        self.size_average = size_average
+    def forward(self, input, target):
+        backend_fn = getattr(self._backend, type(self).__name__)
+        return backend_fn(self.size_average)(input, target)
---
+++ gan_training/BEGAN/modern_main.py
@@ -0,0 +1,211 @@
+import os
+import os.path
+import random
+import argparse
+import numpy as np
+from collections import deque
+
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from torchvision.utils import save_image
+
+from models import *
+from dataloader import *
+
+
+def load_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--z', default=64, type=int)
+    parser.add_argument('--nc', default=64, type=int)
+    parser.add_argument('--batch_size', default=64, type=int)
+    parser.add_argument('--epochs', default=200, type=int)
+    parser.add_argument('--lr', default=0.0001, type=float)
+    parser.add_argument('--lr_lower_boundary', default=2e-6, type=float)
+    parser.add_argument('--gamma', default=0.5, type=float)
+    parser.add_argument('--lambda_k', default=0.001, type=float)
+    parser.add_argument('--k', default=0, type=float)
+    parser.add_argument('--scale', default=64, type=int)
+    parser.add_argument('--model_name', default='test3')
+    parser.add_argument('--base_path', default='./')
+    parser.add_argument('--data_path', default='data/CelebA/images')
+    parser.add_argument('--load_step', default=0, type=int)
+    parser.add_argument('--print_step', default=100, type=int)
+    parser.add_argument('--l_type', default=1, type=int)
+    parser.add_argument('--tanh', default=1, type=int)
+    parser.add_argument('--train', default=1, type=int)
+    opt = parser.parse_args()
+    return args
+
+
+def prepare_paths(args):
+    utils.create_if_empty('./experiments')
+    utils.create_if_empty('./experiments/{}/'.format(args.name))
+    utils.create_if_empty('./experiments/{}/models'.format(args.name))
+    utils.create_if_empty('./experiments/{}/samples'.format(args.name))
+    utils.create_if_empty('./experiments/{}/params'.format(args.name))
+    sample_path = './experiments/{}/samples'.format(args.name)
+    print("Generated samples saved in {}".format(sample_path))
+    return
+
+
+def init_models(args):
+    netG = Generator(args).cuda()
+    netD = Discriminator(args).cuda()
+    print (netG, netD)
+
+    optimG = torch.optim.Adam(netG.parameters(), betas=(0.5, 0.999), lr=args.lr)
+    optimD = torch.optim.Adam(netD.parameters(), betas=(0.5, 0.999), lr=args.lr)
+
+    str = 'experiments/{}/models/'.format(args.model_name)
+    if args.resume_G is not None:
+        netG, optimG = utils.load_model(netG, optimG, str+args.resume_G)
+    if args.resume_D is not None:
+        netD, optimD = utils.load_model(netG, optimD, str+args.resume_D)
+    return (netG, optimG), (netD, optimD)
+
+
+def save_images(args, sample, recon, step, nrow=8):
+    save_path = 'experiments/{}/{}_gen.png'.format(args.model_name, step)
+    save_image(sample, save_path, nrow=nrow, normalize=True)
+    if recon is not None:
+        save_path = 'experiments/{}/{}_disc.png'.format(args.model_name, step)
+        save_image(recon, save_path, nrow=nrow, normalize=True)
+    return
+
+
+def Disc_loss(args, d_real, data, d_fake, g_fake):
+    if args.l_type == 1:
+        real_loss_d = torch.mean(torch.abs(d_real - data))
+        fake_loss_d = torch.mean(torch.abs(d_fake - g_fake))
+    else:
+        real_loss_d = self.criterion(d_real, data)
+        fake_loss_d = self.criterion(d_fake , g_fake.detach())
+    return (real_loss_d, fake_loss_d)
+        
+
+def Gen_loss(args, g_out, g_fake):
+    if args.l_type == 1:
+        return torch.mean(torch.abs(g_out - g_fake))
+    else:
+        return self.criterion(g_out, g_fake)
+
+
+def train(args):
+    random.seed(8722)
+    torch.manual_seed(4565)
+    measure_history = deque([0]*3000, 3000)
+    convergence_history = []
+    prev_measure = 1
+    
+    lr = args.lr
+    iters = args.load_step
+    prepare_paths()
+
+    data_loader = get_loader(args.data_path, args.batch_size, args.scale, 2)
+
+    z = torch.FloatTensor(args.batch_size, args.z).cuda()
+    fixed_z = torch.FloatTensor(args.barch_size, args.z).cuda()
+    fixed_z.data.uniform_(-1, 1)    
+    fixed_x = None
+    
+    self.criterion = L1Loss()
+
+    netG, optimG, netD, optimD = init_models(args)
+    for i in range(args.epochs):
+        for _, data in enumerate(data_loader):
+            if data.size(0) != args.batch_size:
+                continue
+            data = data.cuda()
+            if fixed_x is None:
+                fixed_x = data
+            netD.zero_grad()
+
+            z.data.uniform_(-1, 1)
+            g_fake = netG(z)
+            d_fake = netD(g_fake.detach())
+            d_real = netD(data)
+           
+            real_loss_d, fake_loss_d = Disc_loss(d_real, data, d_fake, g_fake)
+            lossD = real_loss_d - args.k * fake_loss_d
+            lossD.backward()
+            optimD.step()
+        
+            netG.zero_grad()
+            g_fake = netG(z)
+            g_out = netD(g_fake)
+            lossG = Gen_loss(g_out, g_fake)
+            lossG.backward()
+            optimG.step()
+
+            lagrangian = (args.gamma*real_loss_d - fake_loss_d).item()
+            args.k += args.lambda_k * lagrangian
+            args.k = max(min(1, opt.k), 0)
+        
+            convg_measure = real_loss_d.item() + lagrangian.abs()
+            measure_history.append(convg_measure)
+            if iters % args.print_step == 0:
+                print ("Iter: {}, Epoch: {}, D loss: {}, G Loss: {}".format(iter, epoch, 
+                    lossD.item(), lossG.item()))
+                generate_image(args, g_fake, d_real, iter)
+           
+            """update training parameters"""
+            if args.lr_update_type == 1:
+                lr = args.lr * 0.95 ** (iter//3000)
+            elif args.lr_update_type == 2:
+                if iter % 3000 == 3000-1 :
+                    lr *= 0.5
+            elif args.lr_update_type == 3:
+                if iter % 3000 == 3000 -1 :
+                    lr = min(lr*0.5, args.lr_lower_boundary)
+            else:
+                if iter % 3000 == 3000 - 1:
+                    cur_measure = np.mean(measure_history)
+                    if cur_measure > prev_measure * 0.9999:
+                        lr = min(lr*0.5, args.lr_lower_boundary)
+                    prev_measure = cur_measure
+ 
+            for p in optimG.param_groups + optimD.param_groups:
+                p['lr'] = lr
+
+            if iter % 1000 == 0:
+                pathG = 'experiments/{}/models/netG_{}.pt'.format(args.model_name)
+                pathD = 'experiments/{}/models/netD_{}.pt'.format(args.model_name)
+                utils.save_model(pathG, netG, optimG)
+                utils.save_model(pathD, netD, optimD)
+        
+            iter += 1
+
+
+def generative_experiments(args):
+    netG, _, netD, _ = load_models(args)
+    z = []
+    for iter in range(10):
+        z0 = np.random.uniform(-1, 1, args.z)
+        z10 = np.random.uniform(-1, 1, args.z)
+
+        def slerp(val, low, high):
+            low_norm = low/np.linalg.norm(low)
+            high_norm = high/np.linalg.norm(high)
+            omega = np.arccos(np.clip(np.dot(low_norm, high_norm), -1, 1))
+            so = np.sin(omega)
+            if so == 0:
+                return (1.0 - val) * low + val * high # L'Hopital's rule/LERP
+            return np.sin((1.0-val) * omega) / so * low + np.sin(val*omega) / so * high 
+
+        z.append(z0)
+        for i in range(1, 9):
+            z.append(slerp(i*0.1, z0, z10))
+        z.append(z10.reshape(1, args.z)) 
+    z = [_.reshape(1, args.z) for _ in z]
+    z_var = torch.from_numpy(np.concatenate(z, 0)).float().cuda()
+    samples = netG(z_var)
+    save_images(samples, None, 'gen_1014_slerp_{}'.format(args.load_step), 10)
+
+
+if __name__ == "__main__":
+    args = load_args()
+    if opt.train:
+        train(args)
+    else:
+        generative_experiments(args)
