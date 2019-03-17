9b041ea4ed8e0612e2309c319ff378a87eaa3234
neale <nealeratzlaff@gmail.com>
2019-03-16 20:20:06 -0700

    trying cond-began with marginals

--- gan_training/BEGAN/models.py
+++ gan_training/BEGAN/models.py
@@ -3,12 +3,13 @@ import torch.nn as nn
 import torch.nn.functional as F
 
 class Generator(nn.Module):
-    def __init__(self, args, disc=False):
+    def __init__(self, args):
         for k, v in vars(args).items():
             setattr(self, k, v)
         super(Generator, self).__init__()
 
-        self.linear = nn.Linear(self.z, 8*8*self.nc)
+        self.linear = nn.Linear(self.z, 8*8*self.nc-1000)
+        self.linear_m = nn.Linear(9, 1000)
         self.conv_block1 = nn.Sequential(
             nn.Conv2d(self.nc, self.nc, 3, 1, 1),
             nn.ELU(inplace=True),
@@ -35,9 +36,12 @@ class Generator(nn.Module):
         )
         self.last_conv = nn.Conv2d(self.nc, 3, 3, 1, 1)
 
-    def forward(self, input):
+
+    def forward(self, input, marginals):
         x = self.linear(input)
         x = F.elu(x)
+        x_m = F.elu(self.linear_m(marginals.contiguous()))
+        x = torch.cat((x, x_m), 1)
         x = x.view(self.batch_size, self.nc, 8, 8)
         x = self.conv_block1(x)
         x = F.interpolate(x, scale_factor=2)
@@ -49,7 +53,8 @@ class Generator(nn.Module):
         x = self.last_conv(x)
         x = torch.tanh(x)
         return x
-    
+
+
 class Encoder(nn.Module):
     def __init__(self, args):
         for k, v in vars(args).items():
@@ -124,13 +129,44 @@ class Encoder(nn.Module):
         return x
     
 class Discriminator(nn.Module):
-    def __init__(self, nc):
+    def __init__(self, args):
         super(Discriminator, self).__init__()
-        self.enc = Encoder(nc)
-        self.dec = Generator(nc, True)
-    def forward(self, input):
-        return self.dec(self.enc(input))
+        self.enc = Encoder(args)
+        self.dec = Generator(args)
 
+    def forward(self, input, attrs=None):
+        h = self.enc(input)
+        x = self.dec(h, attrs)
+        return x
+
+"""
+class AttributeDetector(nn.Module):
+    def __init__(self, args):
+        for k, v in vars(args).items():
+            setattr(self, k, v)
+        super(AttributeDetector, self).__init__()
+        dim = self.dim
+        self.conv1 = MyConvo2d(3, dim, 3, he_init = False)
+        self.rb1 = ResidualBlock(args, dim, 2*dim, 3, resample='down', hw=dim)
+        self.rb2 = ResidualBlock(args, 2*dim, 4*dim, 3, resample='down', hw=dim//2)
+        self.rb3 = ResidualBlock(args, 4*dim, 8*dim, 3, resample='down', hw=dim//4)
+        self.rb4 = ResidualBlock(args, 8*dim, 8*dim, 3, resample='down', hw=dim//8)
+        self.ln1 = nn.Linear(4*4*8*dim, 9)
+
+    def forward(self, input):
+        output = input.contiguous()
+        output = output.view(-1, 3, 64, 64)
+        output = self.conv1(output)
+        output = self.rb1(output)
+        output = self.rb2(output)
+        output = self.rb3(output)
+        output = self.rb4(output)
+        output = output.view(-1, 4*4*8*self.dim)
+        output = self.ln1(output)
+        output = output.view(output.size(0), -1)
+        #print(output.shape)
+        return output
+"""
 
 def weights_init(self, m):
     classname = m.__class__.__name__
--- gan_training/BEGAN/utils.py
+++ gan_training/BEGAN/utils.py
@@ -5,28 +5,37 @@ import torch
 import glob
 import itertools
 import numpy as np
-
+import pandas as pd
 from scipy.misc import imsave
 import torch.nn as nn
 import torch.nn.init as init
 import torch.distributions.multivariate_normal as N
+import torch.distributions.uniform as U
 from torchvision.utils import save_image
 
+import bayes_net as bn
 
 def sample_z(args, grad=True):
     z = torch.randn(args.batch_size, args.dim, requires_grad=grad).cuda()
     return z
 
 
-def create_d(shape):
+def create_normal(shape):
     mean = torch.zeros(shape)
     cov = torch.eye(shape)
     D = N.MultivariateNormal(mean, cov)
     return D
 
 
-def sample_d(D, shape, scale=1., grad=True):
-    z = scale * D.sample((shape,)).cuda()
+def create_uniform(min, max):
+    min = torch.tensor([min]).float()
+    max = torch.tensor([max]).float()
+    D = U.Uniform(min, max)
+    return D
+
+
+def sample_z(D, shape, scale=1., grad=True):
+    z = scale * D.sample((shape)).cuda()
     z.requires_grad = grad
     return z
 
@@ -65,6 +74,96 @@ def load_net_only(model, d):
     return model
 
 
+def get_marginals(graph, batch_size):
+    df = pd.DataFrame(columns=bn.KEEP_ATTS)
+    # only need to generate one set of random evidence per batch
+    evidence = bn.random_evidence()
+    targets = []
+    for val in bn.KEEP_ATTS:
+        if val not in evidence.keys():
+            targets.append(val)
+    query = bn.graph_inference(graph, targets, evidence)
+
+    # just repeat it for the entire batch, since we want to pass the same evidence in
+    for i in range(batch_size):
+        for val in bn.KEEP_ATTS:
+            if val not in targets:
+                df.loc[i, val] = 1
+            else:
+                df.loc[i, val] = query[val].values[1]
+                #print(val, query[val].values[1])
+
+    df = df.apply(pd.to_numeric, downcast='float', errors='coerce')
+    return df.values
+
+
+"""
+def load_feature_detector(args):
+    print('Loading feature detector...\n')
+    fdet = AttributeDetector(args).cuda()
+    fdet.load_state_dict(torch.load('saved_models/celeba/netFDET_best_r2'))
+    for p in fdet.parameters():
+        p.requires_grad = False
+    fdet.eval()
+    return fdet
+"""
+
+def evidence_test(args):
+    torch.manual_seed(8734)
+    print('Evidence test...\n')
+    netG = Generator(args).cuda()
+    netD = Discriminator(args).cuda()
+    fdet = load_feature_detector(args)
+    graph = bn.create_bayes_net()
+    evidence = bn.evidence_query(['Young', 'Glasses'], [1, 1])
+    marginals = bn.return_marginals(graph, args.batch_size, evidence)
+
+    bce_loss = BCEWithLogitsLoss()
+    noise = torch.randn(args.batch_size, args.z).cuda()
+    if args.dist_exp == 1:
+        print('Loading models...\n')
+        netG_saved = torch.load('./saved_models/celeba_resnet/netG_current_dist')
+        netD_saved = torch.load('./saved_models/celeba_resnet/netD_current_dist')
+        netG.load_state_dict(netG_saved['state_dict'])
+        netD.load_state_dict(netD_saved['state_dict'])
+
+        with torch.no_grad():
+            marginals = torch.tensor(marginals).cuda()
+            fake = netG(noise, marginals)
+            G, _ = netD(fake)
+            G_attrs = torch.sigmoid(fdet(fake))
+            classif_loss = torch.sum(torch.min(G_attrs, 1-G_attrs))
+            G_attrs = torch.round(torch.sigmoid(G_attrs)).mean(0)
+            dist_loss = mseloss(G_attrs, marginals[0])
+            path = 'results/celeba_resnet/evtest_dist.png'
+            utils.generate_image(args, marginals, netG, path)
+
+            print('Distribution Loss', dist_loss.cpu().item())
+            print('Classification Loss', classif_loss.cpu().item())
+
+    else:
+        print('Loading models...\n')
+        netG_saved = torch.load('./saved_models/celeba_resnet/netG_current')
+        netD_saved = torch.load('./saved_models/celeba_resnet/netD_current')
+        netG.load_state_dict(netG_saved['state_dict'])
+        netD.load_state_dict(netD_saved['state_dict'])
+        optimG.load_state_dict(netG_saved['optimizer'])
+        optimD.load_state_dict(netD_saved['optimizer'])
+
+        with torch.no_grad():
+            marginals = torch.tensor(marginals)
+            mdist = torch.distributions.Bernoulli(marginals)
+            attr_samples = mdist.sample().cuda()
+            fake = netG(noise, attr_samples)
+            G, _ = netD(fake)
+            fake_preds = torch.sigmoid(fdet(fake))
+            G_attrs = bce_loss(fake_preds, attr_samples)
+            path = 'results/celeba_resnet/evtest_sampling.png'
+            utils.generate_image(args, attr_samples, netG, path)
+
+            print('Attribute Loss', G_attrs.cpu().item())
+
+
 def generate_image(args, netG, path):
     with torch.no_grad():
         noise = torch.randn(args.batch_size, args.z, requires_grad=True).cuda()
@@ -75,6 +174,6 @@ def generate_image(args, netG, path):
     else:
         channels = samples.shape[1]
         samples = samples.view(-1, channels, 64, 64)
-       	samples = samples.mul(0.5).add(0.5) 
+        samples = samples.mul(0.5).add(0.5) 
     print ('saving sample: ', path)
     save_image(samples, path)
